6eec2aba177aa8e8a263081991b645076784152c
luke-robinett <lukebootcamp@gmail.com>
2020-03-28 14:05:36 -0700

    Removed unneeded files and built basic html skeleton in app.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <h1>Portfolio</h1>
       </header>
+      <nav>
+        <p>Nav stuff will be here</p>
+      </nav>
+      <main>
+        <h2>Main Stuff Here</h2>
+      </main>
+      <footer>
+        2020 by Luke Robinett
+      </footer>
     </div>
   );
 }
