dae35add9068fbbcb5336f9d4bf0bd6fee7a71d4
luke-robinett <lukebootcamp@gmail.com>
2020-03-28 22:24:42 -0700

    Moved BrowserRouter tag back into app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route } from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 import NavBar from "./components/navbar";
 import About from "./pages/about";
 import Gallery from "./pages/Gallery";
@@ -8,21 +8,23 @@ import Footer from "./components/footer";
 
 function App() {
   return (
-    <div>
-      <NavBar />
+    <BrowserRouter>
+      <div>
+        <NavBar />
 
-      <main className="container mt-3">
-        <Route exact path="/" component={About} />
-        <Route exact path="/about" component={About} />
-        <Route exact path="/gallery" component={Gallery} />
-        <Route exact path="/contact" component={Contact} />
-      </main>
+        <main className="container mt-3">
+          <Route exact path="/" component={About} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/gallery" component={Gallery} />
+          <Route exact path="/contact" component={Contact} />
+        </main>
 
-      <Footer
-        date="2020"
-        name="Luke Robinett"
-      />
-    </div>
+        <Footer
+          date="2020"
+          name="Luke Robinett"
+        />
+      </div>
+    </BrowserRouter>
   );
 }
 
