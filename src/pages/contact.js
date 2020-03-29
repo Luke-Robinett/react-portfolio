743d71a64342fa907b17f5796e4700b865c2e084
luke-robinett <lukebootcamp@gmail.com>
2020-03-28 19:56:52 -0700

    Created Contact page component

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,9 +1,21 @@
 import React from "react";
-import pic from "../assets/img/luke.jpg";
 
-function About() {
+function Contact() {
   return (
+    <div>
+      <h1>Contact</h1>
+      <hr />
+      <h2 className="mt-4">LinkedIn</h2>
+      <i className="fa fa-linkedin fa-2x"></i>
+      <a href="https://www.linkedin.com/in/lucas-robinett/" target="_blank">View my LinkedIn Profile</a>
+      <h2 className="mt-4">GitHub</h2>
+      <i className="fa fa-github fa-2x"></i>
+      <a href="https://github.com/luke-robinett" target="_blank">View my GitHub Account</a>
+      <h2 className="mt-4">Email</h2>
+      <i className="fa fa-envelope fa-2x"></i>
+      <a href="mailto:info@lukerobinett.com">info@lukerobinett.com</a>
+    </div>
   )
 }
 
-export default About;
+export default Contact;
